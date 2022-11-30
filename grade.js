871169027e53c539cfaa8d5bad975f4640047497
REACTClass <21jamoon@gmail.com>
2022-11-29 19:41:30 -0500

    first commit

---
+++ grade.js
@@ -0,0 +1,15 @@
+let grade = (40);
+if (grade >= 80) {
+    console.log("You did a great job");
+}else if (grade >=70 && grade <80) {
+    console.log("Keep trying");
+} else if (grade >=60 && grade < 70) {
+    console.log("ehhh");
+} else if (grade >55 && grade < 60) {
+   console.log("Not too good");
+} else  {
+   console.log("You'll get them next time");
+                }
+
+
+ 
